2577009af443a1e96999a72a756d19ae4867f40a
Fmquito45 <fmquitox45@gmail.com>
2023-10-04 00:30:35 -0400

    adds dependencies to js file

---
+++ script.js
@@ -0,0 +1,6 @@
+var searchInputEl = document.getElementById("search-input");
+var searchButtonEl = document.getElementById("search-button");
+var cityEl = document.getElementById("city");
+
+var apiKey = f1857ee728cff532cf0b670e7ef214c4
+
